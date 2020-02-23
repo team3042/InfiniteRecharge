3e4a435ebb3d30b1a344d00dbe7023664dc02167
Anthony Brogni <anthonybrog@gmail.com>
2020-02-23 17:14:58 -0600

    backwards to forwards

--- src/main/java/org/usfirst/frc/team3042/robot/commands/AutonomousMode.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/AutonomousMode.java
@@ -3,14 +3,14 @@ package org.usfirst.frc.team3042.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /** Autonomous Mode ******************************************************
- * Autonomous Mode for driving backwards and then shooting the three pre-loaded balls
+ * Autonomous Mode for driving forwards and then shooting the three pre-loaded balls
  */
 public class AutonomousMode extends CommandGroup {
 
   public AutonomousMode() {
     addParallel(new Shooter_Spin()); //Spin up the shooter
     addParallel(new Turret_Continous()); //Search for the target and start tracking it
-    addSequential(new Drivetrain_GyroStraight(10.0, 80.0)); //Drive backwards off the initiation line
+    addSequential(new Drivetrain_GyroStraight(10.0, 80.0)); //Drive forwards off the initiation line
     addSequential(new Shoot(true)); //Shoot the three pre-loaded power cells
     addSequential(new Turret_Stop()); //Stop tracking the target, stop spinning the shooter, and turn off the Limelight's LEDs
   }
--- src/main/java/org/usfirst/frc/team3042/robot/commands/AutonomousMode_Delayed.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/AutonomousMode_Delayed.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team3042.robot.commands;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 /** Autonomous Mode Delayed ******************************************************
- * Autonomous Mode for driving backwards and then shooting the three pre-loaded balls after waiting a certain number of seconds
+ * Autonomous Mode for driving forwards and then shooting the three pre-loaded balls after waiting a certain number of seconds
  */
 public class AutonomousMode_Delayed extends CommandGroup {
 
@@ -11,7 +11,7 @@ public class AutonomousMode_Delayed extends CommandGroup {
     addParallel(new Shooter_Spin()); //Spin up the shooter
     addParallel(new Turret_Continous()); //Search for the target and start tracking it
     addSequential(new Wait(10)); //Wait 10 seconds
-    addSequential(new Drivetrain_GyroStraight(10.0, 80.0)); //Drive backwards off the initiation line
+    addSequential(new Drivetrain_GyroStraight(10.0, 80.0)); //Drive forwards off the initiation line
     addSequential(new Shoot(true)); //Shoot the three pre-loaded power cells
     addSequential(new Turret_Stop()); //Stop tracking the target, stop spinning the shooter, and turn off the Limelight's LEDs
   }
