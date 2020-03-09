a92d80b9a4a3536c2a4010cf15ad49f260109632
Anthony Brogni <anthonybrog@gmail.com>
2020-03-09 16:45:07 -0500

    Autonomous Edits

--- src/main/java/org/usfirst/frc/team3042/robot/commands/AutonomousMode_Delayed.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/AutonomousMode_Delayed.java
@@ -10,9 +10,8 @@ public class AutonomousMode_Delayed extends CommandGroup {
   public AutonomousMode_Delayed() {
     addParallel(new Shooter_Spin()); //Spin up the shooter
     addParallel(new Turret_Continous(true)); //Search for the target and start tracking it
-    addSequential(new Wait(8)); //Wait 10 seconds
+    addSequential(new Wait(10)); //Wait 10 seconds
     addSequential(new Drivetrain_GyroStraight(12.0, -80.0)); //Drive forwards off the initiation line
     addSequential(new Shoot()); //Shoot the three pre-loaded power cells
-    addSequential(new Turret_Stop()); //Stop tracking the target, stop spinning the shooter, and turn off the Limelight's LEDs
   }
 }
