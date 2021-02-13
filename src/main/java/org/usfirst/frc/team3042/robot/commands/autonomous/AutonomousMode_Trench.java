92af83e127e58ae2cf79af841506bebe0d1b7973
Anthony Brogni <anthonybrog@gmail.com>
2021-02-13 14:23:24 -0600

    Remove Intake_Stop
    Unnecessary command, can just use Intake_Intake(0) instead

--- src/main/java/org/usfirst/frc/team3042/robot/commands/autonomous/AutonomousMode_Trench.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/autonomous/AutonomousMode_Trench.java
@@ -3,7 +3,6 @@ package org.usfirst.frc.team3042.robot.commands.autonomous;
 import org.usfirst.frc.team3042.robot.commands.Drivetrain_GyroStraight;
 import org.usfirst.frc.team3042.robot.commands.Drivetrain_GyroTurn;
 import org.usfirst.frc.team3042.robot.commands.Intake_Intake;
-import org.usfirst.frc.team3042.robot.commands.Intake_Stop;
 import org.usfirst.frc.team3042.robot.commands.Shoot;
 import org.usfirst.frc.team3042.robot.commands.Shooter_Spin;
 import org.usfirst.frc.team3042.robot.commands.Turret_Continous;
@@ -24,7 +23,7 @@ public class AutonomousMode_Trench extends CommandGroup {
     addSequential(new Drivetrain_GyroTurn(-90)); //Spin to knock down the intake
     addParallel(new Intake_Intake(1)); //Start running the intake
     addSequential(new Drivetrain_GyroStraight(24.0, -40.0)); //Drive forwards into the trench
-    addSequential(new Intake_Stop()); //Stop running the intake
+    addSequential(new Intake_Intake(0)); //Stop running the intake
     addSequential(new Drivetrain_GyroStraight(24.0, 80.0)); //Drive back to the optimal shooting location
     addSequential(new Shoot()); //Shoot the three power cells from the trench
   }
