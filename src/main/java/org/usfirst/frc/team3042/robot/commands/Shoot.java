04379daf688fdeb9dabd935a3e059516fed49b6b
Anthony Brogni <anthonybrog@gmail.com>
2021-02-13 14:40:05 -0600

    updated to new shoot command

--- src/main/java/org/usfirst/frc/team3042/robot/commands/Shoot.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/Shoot.java
@@ -14,67 +14,74 @@ import org.usfirst.frc.team3042.robot.subsystems.UpperConveyor;
 /** Shoot *********************************************************************
  * Command for pushing power cells into the shooter wheels */
 public class Shoot extends Command {
-    /** Configuration Constants ***********************************************/
-    private static final Log.Level LOG_LEVEL = RobotMap.LOG_UPPER_CONVEYOR;
-    private static final double LPOWER = RobotMap.LOWER_CONVEYOR_POWER;
-    private static final double UPOWER = RobotMap.UPPER_CONVEYOR_POWER;
-    private static final double SPEED = RobotMap.SHOOTER_VELOCITY;
-    private static final double TOLERANCE = RobotMap.TURRET_ANGLE_TOLERANCE;
+  /** Configuration Constants ***********************************************/
+  private static final Log.Level LOG_LEVEL = RobotMap.LOG_UPPER_CONVEYOR;
+  private static final double LPOWER = RobotMap.LOWER_CONVEYOR_POWER;
+  private static final double UPOWER = RobotMap.UPPER_CONVEYOR_POWER;
+  private static final double SPEED = RobotMap.SHOOTER_VELOCITY;
+  private static final double TOLERANCE = RobotMap.TURRET_ANGLE_TOLERANCE;
 
-    /** Instance Variables ****************************************************/
-    UpperConveyor upperconveyor = Robot.upperconveyor;
-    LowerConveyor lowerconveyor = Robot.lowerconveyor;
-    Shooter shooter = Robot.shooter;
-    Limelight limelight = Robot.limelight;
-    Log log = new Log(LOG_LEVEL, SendableRegistry.getName(upperconveyor));
+  /** Instance Variables ****************************************************/
+  UpperConveyor upperconveyor = Robot.upperconveyor;
+  LowerConveyor lowerconveyor = Robot.lowerconveyor;
+  Shooter shooter = Robot.shooter;
+  Limelight limelight = Robot.limelight;
+  Log log = new Log(LOG_LEVEL, SendableRegistry.getName(upperconveyor));
+  boolean onTarget = false;
 
-    /** Shoot *****************************************************************
-     * Required subsystems will cancel commands when this command is run. */
-    public Shoot() {
-      log.add("Constructor", Log.Level.TRACE);
+  /** Shoot *****************************************************************
+   * Required subsystems will cancel commands when this command is run. */
+  public Shoot() {
+    log.add("Constructor", Log.Level.TRACE);
 
-      requires(upperconveyor);
-      requires(lowerconveyor);
+    requires(upperconveyor);
+    requires(lowerconveyor);
+  }
+
+  /** initialize **************************************************************
+   * Called just before this Command runs the first time */
+  protected void initialize() {
+    log.add("Initialize", Log.Level.TRACE);
+    onTarget = false;
+  }
+
+  /** execute ***************************************************************
+   * Called repeatedly when this Command is scheduled to run */
+  protected void execute() {
+    if (!onTarget && limelight.returnValidTarget() == 1.0 && Math.abs(limelight.returnHorizontalError()) <= TOLERANCE && shooter.getSpeed() >= SPEED) {
+      lowerconveyor.setPower(LPOWER);
+      upperconveyor.setPower(UPOWER);
+      onTarget = true;
     }
-
-	  /** initialize **************************************************************
-     * Called just before this Command runs the first time */
-    protected void initialize() {
-      log.add("Initialize", Log.Level.TRACE);
+    else if (onTarget && shooter.getSpeed() >= SPEED) {
+      lowerconveyor.setPower(LPOWER);
+      upperconveyor.setPower(UPOWER);
     }
-
-    /** execute ***************************************************************
-     * Called repeatedly when this Command is scheduled to run */
-    protected void execute() {
-      if (limelight.returnValidTarget() == 1.0 && Math.abs(limelight.returnHorizontalError()) <= TOLERANCE && shooter.getSpeed() >= SPEED) {
-        lowerconveyor.setPower(LPOWER);
-        upperconveyor.setPower(UPOWER);
-      }
-      else {
-        upperconveyor.stop();
-        lowerconveyor.stop();
-      }
-    }
-    
-    /** isFinished ************************************************************	
-     * Make this return true when this Command no longer needs to run execute() */
-    protected boolean isFinished() {
-      return false;
-    }
-    
-    /** end *******************************************************************
-     * Called once after isFinished returns true */
-    protected void end() {
-      log.add("End", Log.Level.TRACE);
+    else if (shooter.getSpeed() < SPEED) {
       upperconveyor.stop();
       lowerconveyor.stop();
     }
+  }
+  
+  /** isFinished ************************************************************	
+   * Make this return true when this Command no longer needs to run execute() */
+  protected boolean isFinished() {
+    return false;
+  }
+  
+  /** end *******************************************************************
+   * Called once after isFinished returns true */
+  protected void end() {
+    log.add("End", Log.Level.TRACE);
+    upperconveyor.stop();
+    lowerconveyor.stop();
+  }
 
-    /** interrupted ***********************************************************
-     * Called when another command which requires one or more of the same subsystems is scheduled to run */
-    protected void interrupted() {
-      log.add("Interrupted", Log.Level.TRACE);
-      upperconveyor.stop();
-      lowerconveyor.stop();
-    }
+  /** interrupted ***********************************************************
+   * Called when another command which requires one or more of the same subsystems is scheduled to run */
+  protected void interrupted() {
+    log.add("Interrupted", Log.Level.TRACE);
+    upperconveyor.stop();
+    lowerconveyor.stop();
+  }
 }
