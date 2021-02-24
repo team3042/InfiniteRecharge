0154faf713e4ad2d7da2b56887eeb7fd1c992785
Anthony Brogni <anthonybrog@gmail.com>
2021-02-24 09:14:40 -0600

    Make exitTrench a reversed enterTrench

--- src/main/java/org/usfirst/frc/team3042/robot/paths/exitTrench.java
+++ src/main/java/org/usfirst/frc/team3042/robot/paths/exitTrench.java
@@ -13,21 +13,21 @@ public class exitTrench implements PathContainer {
     @Override
     public Path buildPath() {
         ArrayList<Waypoint> waypoints = new ArrayList<Waypoint>();
-        waypoints.add(new Waypoint(318,-27,0,0));
-        waypoints.add(new Waypoint(221,-27,0,60));
+        waypoints.add(new Waypoint(318,-27,0,60));
+        waypoints.add(new Waypoint(221,-27,15,60));
         waypoints.add(new Waypoint(183,-66,15,60));
-        waypoints.add(new Waypoint(132,-96,15,60));
-
+        waypoints.add(new Waypoint(132,-96,0,60));
+        
         return PathUtil.buildPathFromWaypoints(waypoints);
     }
     
     @Override
     public RigidTransform2d getStartPose() {
-        return new RigidTransform2d(new Translation2d(318,-27), Rotation2d.fromDegrees(180)); 
+        return new RigidTransform2d(new Translation2d(132,-96), Rotation2d.fromDegrees(180)); 
     }
 
     @Override
     public boolean isReversed() {
-        return false; 
+        return true; 
     }
 }
