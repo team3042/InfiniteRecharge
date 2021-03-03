730757e981cf02ef6f7a6f208c92a62b2f236ed3
Anthony Brogni <anthonybrog@gmail.com>
2021-03-03 14:21:25 -0600

    Update AutonomousMode_Trench.java

--- src/main/java/org/usfirst/frc/team3042/robot/commands/autonomous/AutonomousMode_Trench.java
+++ src/main/java/org/usfirst/frc/team3042/robot/commands/autonomous/AutonomousMode_Trench.java
@@ -16,7 +16,7 @@ public class AutonomousMode_Trench extends CommandGroup {
   public AutonomousMode_Trench() {
     addParallel(new Turret_Continous(true)); //Start tracking the target with the turret
     addParallel(new Shooter_Spin()); //Spin up the shooter
-    addSequential(new Drivetrain_GyroStraight(12.0, 120.0)); //Drive forwards off the initiation line
+    addSequential(new Drivetrain_GyroStraight(12.0, 100.0)); //Drive forwards off the initiation line
     addSequential(new Shoot(3)); //Shoot the 3 pre-loaded power cells; first parameter is time in seconds
     addSequential(new Turret_Stop()); //Stop tracking the target and running the shooter when we don't need to
 
@@ -33,7 +33,6 @@ public class AutonomousMode_Trench extends CommandGroup {
     addSequential(new Drivetrain_GyroStraight(100, 60));
 
     addSequential(new Intake_Intake(0)); //Stop running the intake
-    addParallel(new Shooter_Spin()); //Spin up the shooter
 
     // Drive back to the optimal shooting location //
     addSequential(new Drivetrain_GyroStraight(100, -100));
@@ -41,7 +40,8 @@ public class AutonomousMode_Trench extends CommandGroup {
     addSequential(new Drivetrain_GyroStraight(82, -100));
     addSequential(new Drivetrain_GyroTurn(70));
 
-    addParallel(new Turret_Continous(true)); //Start tracking the target with the turret
-    addSequential(new Shoot()); //Shoot the 3 power cells we collected from the trench
+    //addParallel(new Shooter_Spin()); //Spin up the shooter
+    //addParallel(new Turret_Continous(true)); //Start tracking the target with the turret
+    //addSequential(new Shoot()); //Shoot the 3 power cells we collected from the trench
   }
 }
