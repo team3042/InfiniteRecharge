073c0842deb4a12da7b6f2f862c5641561856c82
Anthony Brogni <anthonybrog@gmail.com>
2021-03-17 13:28:39 -0500

    convert drivetrain subsystem

--- src/main/java/org/usfirst/frc/team3042/robot/subsystems/Drivetrain.java
+++ src/main/java/org/usfirst/frc/team3042/robot/subsystems/Drivetrain.java
@@ -2,57 +2,79 @@ package org.usfirst.frc.team3042.robot.subsystems;
 
 import org.usfirst.frc.team3042.lib.Log;
 import org.usfirst.frc.team3042.robot.RobotMap;
-import org.usfirst.frc.team3042.robot.commands.Drivetrain_TankDrive;
+import org.usfirst.frc.team3042.robot.commands.drivetrain.Drivetrain_TankDrive;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
+import edu.wpi.first.wpilibj.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveWheelSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SendableRegistry;
 
 /** Drivetrain ****************************************************************
- * The drivetrain subsystem for the robot. */
+ * The drivetrain subsystem of the robot. */
 public class Drivetrain extends Subsystem {
 	/** Configuration Constants ***********************************************/
 	private static final Log.Level LOG_LEVEL = RobotMap.LOG_DRIVETRAIN;
 	private static final int CAN_LEFT_MOTOR = RobotMap.CAN_LEFT_MOTOR;
 	private static final int CAN_RIGHT_MOTOR = RobotMap.CAN_RIGHT_MOTOR;
+	private static final int CAN_LEFT_FOLLOWER = RobotMap.CAN_LEFT_FOLLOWER;
+	private static final int CAN_RIGHT_FOLLOWER = RobotMap.CAN_RIGHT_FOLLOWER;
+	private static final double kP = RobotMap.kP_DRIVE;
+	private static final double ks = RobotMap.ks_DRIVE;
+	private static final double kv = RobotMap.kv_DRIVE;
 	private static final NeutralMode BRAKE_MODE = RobotMap.DRIVETRAIN_BRAKE_MODE;
 	private static final boolean REVERSE_LEFT_MOTOR = RobotMap.REVERSE_LEFT_MOTOR;
 	private static final boolean REVERSE_RIGHT_MOTOR = RobotMap.REVERSE_RIGHT_MOTOR;	
-	private static final int SOLENOID_ID = RobotMap.DRIVETRAIN_SOLENOID;
 	
 	/** Instance Variables ****************************************************/
 	Log log = new Log(LOG_LEVEL, SendableRegistry.getName(this));
-	TalonSRX leftMotor = new TalonSRX(CAN_LEFT_MOTOR);
-	TalonSRX rightMotor = new TalonSRX(CAN_RIGHT_MOTOR);
-	Solenoid shifter = new Solenoid(SOLENOID_ID);
 
-	DrivetrainFollowers followers;
+	WPI_TalonSRX leftMotor = new WPI_TalonSRX(CAN_LEFT_MOTOR);
+	WPI_TalonSRX rightMotor = new WPI_TalonSRX(CAN_RIGHT_MOTOR);
+	WPI_TalonSRX leftFollower = new WPI_TalonSRX(CAN_LEFT_FOLLOWER);
+	WPI_TalonSRX rightFollower = new WPI_TalonSRX(CAN_RIGHT_FOLLOWER);	
+
+	private final PIDController leftPIDController = new PIDController(kP, 0.0, 0.0);
+	private final PIDController rightPIDController = new PIDController(kP, 0.0, 0.0);
+	
+	private final SimpleMotorFeedforward feedforward = new SimpleMotorFeedforward(ks, kv);
+
+	Gyro gyroscope = new ADXRS450_Gyro(); // The gyroscope sensor
+	DifferentialDriveKinematics kinematics = new DifferentialDriveKinematics(RobotMap.ROBOT_WIDTH / 39.3700787); // Divide by 39.3700787 to convert inches to meters
+
 	DrivetrainEncoders encoders;
-	DrivetrainAuton auton;
-
-	Boolean isHighGear = false;
+	DifferentialDriveOdometry odometry; // Odometry class for tracking robot posistion
 	
 	/** Drivetrain ************************************************************
 	 * Set up the talons for desired behavior. */
 	public Drivetrain() {
 		log.add("Constructor", LOG_LEVEL);
 		
-		followers = new DrivetrainFollowers();
 		encoders = new DrivetrainEncoders(leftMotor, rightMotor);
-		auton = new DrivetrainAuton(leftMotor, rightMotor, encoders);
-		
+
+		leftFollower.set(ControlMode.Follower, CAN_LEFT_MOTOR);
+		rightFollower.set(ControlMode.Follower, CAN_RIGHT_MOTOR);
+
 		initMotor(leftMotor, REVERSE_LEFT_MOTOR);
 		initMotor(rightMotor, REVERSE_RIGHT_MOTOR);
+		initMotor(leftFollower, REVERSE_LEFT_MOTOR);
+		initMotor(rightFollower, REVERSE_RIGHT_MOTOR);
 
-		setLowGear();
+		odometry = new DifferentialDriveOdometry(gyroscope.getRotation2d());
 	}
-	private void initMotor(TalonSRX motor, boolean reverse) {
+	private void initMotor(WPI_TalonSRX motor, boolean reverse) {
 		motor.setNeutralMode(BRAKE_MODE);
-		motor.setInverted(reverse); 	// affects percent Vbus mode
+		motor.setInverted(reverse);
 	}
 	
 	/** initDefaultCommand ****************************************************
@@ -69,7 +91,7 @@ public class Drivetrain extends Subsystem {
 		leftMotor.set(ControlMode.PercentOutput, leftPower);
 		rightMotor.set(ControlMode.PercentOutput, rightPower);		
 	}
-	public void stop() {
+	public void stop() { // Stop the drivetrain
 		setPower(0.0, 0.0);
 	}
 	private double safetyCheck(double power) {
@@ -78,29 +100,56 @@ public class Drivetrain extends Subsystem {
 		return power;
 	}
 
-	/* Methods for the pneumatic gearbox shifter */
-	public void setHighGear(){
-    	shifter.set(true);
-    	isHighGear = true;
-    }
-    public void setLowGear(){
-    	shifter.set(false);
-    	isHighGear = false;
-    }
-    public void toggleGear(){
-    	if (isHighGear){
-    		setLowGear();
-    	}
-    	else {
-    		setHighGear();
-    	}
-    }
+	/** Odometry Methods *******************************************************/
+  	public void updateOdometry() { // Updates the field-relative position.
+    	odometry.update(gyroscope.getRotation2d(), positionToMeters(encoders.getLeftPosition()), positionToMeters(encoders.getRightPosition()));
+  	}
+  	public void resetOdometry(Pose2d pose) { // Resets the field-relative position to a specific location.
+    	odometry.resetPosition(pose, gyroscope.getRotation2d());
+  	}
+  	public Pose2d getPose() { // Returns the position of the robot.
+    	return odometry.getPoseMeters();
+	  }
+	  
+	//Conversion Methods: Convert to Meters
+	public double positionToMeters(double position) {
+		return position * Math.PI * RobotMap.WHEEL_DIAMETER / 39.3700787; // Divide by 39.3700787 to convert inches to meters
+	}
+	public double speedToMeters(double speed) {
+		return speed / 60 * Math.PI * RobotMap.WHEEL_DIAMETER / 39.3700787; // Divide by 39.3700787 to convert inches to meters
+	}
+
+	/** Gyroscope Methods *******************************************************/
+  	public void zeroGyro() { // Zeroes the heading of the robot
+    	gyroscope.reset();
+	  }
+	public double getAngle() { // Returns the heading of the robot
+		return gyroscope.getRotation2d().getDegrees();
+	}
+	public double getTurnRate() { // Returns the turn rate of the robot
+		return -gyroscope.getRate();
+	}
+
+	// Drives the robot with the given linear velocity and angular velocity.
+  	@SuppressWarnings("ParameterName")
+  	public void drive(double xSpeed, double rot) { 
+    	var wheelSpeeds = kinematics.toWheelSpeeds(new ChassisSpeeds(xSpeed, 0.0, rot));
+    	setSpeeds(wheelSpeeds);
+	}
+	// Sets the desired wheel speeds.
+  	public void setSpeeds(DifferentialDriveWheelSpeeds speeds) { 
+    	final double leftFeedforward = feedforward.calculate(speeds.leftMetersPerSecond);
+    	final double rightFeedforward = feedforward.calculate(speeds.rightMetersPerSecond);
+
+    	final double leftOutput = leftPIDController.calculate(speedToMeters(encoders.getLeftSpeed()), speeds.leftMetersPerSecond);
+		final double rightOutput = rightPIDController.calculate(speedToMeters(encoders.getRightSpeed()), speeds.rightMetersPerSecond);
+		
+    	leftMotor.setVoltage(leftOutput + leftFeedforward);
+   		rightMotor.setVoltage(rightOutput + rightFeedforward);
+  	}
 	
-	/** Provide commands access to the encoders and autonomous ****************/
+	/** Give commands access to the drivetrain encoders ****************/
 	public DrivetrainEncoders getEncoders() {
 		return encoders;
-	}
-	public DrivetrainAuton getAuton() {
-		return auton;
 	}
 }
